5a45249a49db83c93c0ab1ba14b93cb84dc73ef6
Ildar Akhmetgaleev <akhilman@gmail.com>
2017-01-26 15:56:39 +0700

    Fixes for colors

--- functions/fish_hline_prompt.fish
+++ functions/fish_hline_prompt.fish
@@ -34,7 +34,7 @@ function fish_hline_prompt
     end
 
     set -l normal (set_color normal)
-    set prompt_left (set_color $fish_color_host) "(" (set_color $color_cwd) (prompt_pwd) (set_color $fish_color_host) ")" $normal
+    set prompt_left (set_color $fish_color_host) "(" $normal (set_color $color_cwd) (prompt_pwd) $normal (set_color $fish_color_host) ")" $normal
     set prompt_right (set_color $fish_color_host) "(" $normal (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" (set_color $fish_color_host) ")" $normal
 
     echo -sn (set_color $fish_color_host) ".-" $prompt_left (set_color $fish_color_host) $prompt_line $prompt_right (set_color $fish_color_host) "-" $normal
